91832922f3faec22c06922dbb9edcd78f5e2a596
Clint Glenane <clint@terrabit.co.za>
2011-04-08 07:37:51 +0200

    Updates HUD comments

--- GemBoy/PlatformerGame.cs
+++ GemBoy/PlatformerGame.cs
@@ -207,7 +207,7 @@ namespace GemBoy
             float timeHeight = hudFont.MeasureString(timeString).Y;
             DrawShadowedString(hudFont, "SCORE: " + level.Score.ToString(), hudLocation + new Vector2(0.0f, timeHeight * 1.2f), Color.Yellow);
 
-                        // Determine the status overlay message to show.
+            // Determine the status overlay message to show.
             Texture2D status = null;
             if (level.TimeRemaining == TimeSpan.Zero)
             {
